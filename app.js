d6687972b479db4529572a1f20176cfc66c75db4
makelele29 <jacastillo.ugr@gmail.com>
2016-11-09 19:11:25 +0100

    Añadida url de mongo

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', routes);
 app.use('/users', users);
 // connect to our database
 var uristring =
-    process.env.MONGOLAB_URI ||
+    'mongodb://heroku_zv0pxkrz:heroku_zv0pxkrz@ds147487.mlab.com:47487/heroku_zv0pxkrz' ||
     'mongodb://localhost:27017/porra';
 
 // The http server will listen to an appropriate port, or default to
